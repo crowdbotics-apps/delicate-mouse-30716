8790cb6a8bb65770a42c1428f571db9d5a2540fe
Crowdbotics <team@crowdbotics.com>
2021-09-21 02:56:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "delicate_mouse_30716Identifier",
+  url: "https://delicate_mouse_30716Identifier.botics.co",
+  api: "https://delicate_mouse_30716Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
